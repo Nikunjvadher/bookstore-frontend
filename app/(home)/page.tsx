b1dde877e7a1e5151f1b4ebc08de5cef1bfa062f
Nikunj Vadher <nikunjjv27@gmail.com>
2026-02-12 22:00:24 +0530

    updated

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Banner from './components/Banner'
-import Card from './components/Card'
+// import Card from './components/Card'
+import BookList from './components/BookList'
 
-const page = () => {
+const page = async () => {
+
+  const response = await fetch(`${process.env.API_URL}/books`)
+  const book = response.json();
+  console.log(response)
+  console.log(book)
+
+
   return (
     <div>
       <Banner />
-      <Card />
+      {/* <Card /> */}
+      <BookList />
     </div>
   )
 }

db50c2b14f5ed29438871970663fcc3f7e7a026b
Nikunj Vadher <nikunjjv27@gmail.com>
2026-02-14 19:39:41 +0530

    update card ui

--- app/(home)/components/Card.tsx
+++ app/(home)/components/Card.tsx
@@ -5,11 +5,11 @@ import Link from 'next/link'
 
 const Card = ({ book }: { book: Book }) => {
   return (
-    <div className='flex w-120 max-w-fit border rounded-md p-5 shadow-md  gap-5 flex-1'>
-      <Image src={book.coverImage} alt={book.title} height={0} width={0} sizes='100vw' style={{width:"12rem" , height:"auto"}} loading='lazy' />
+    <div className='flex w-125  border rounded-md p-5 shadow-md  gap-5 flex-1'>
+      <Image src={book.coverImage} alt={book.title} height={0} width={0} sizes='100vw' style={{width:"10rem" , height:"auto"}} loading='lazy' />
       <div className="flex flex-col gap-2">
         <h3 className='text-xl text-orange-600 font-bold line-clamp-2 '>{book.title}</h3>
-        <p className=''>{book.author.name}</p>
+        <p className='text-xl font-bold line-clamp-2 '>{book.author.name}</p>
         <Link href={`/books/${book._id}`} className='rounded-md border px-2 py-2 w-fit  border-amber-600 hover:bg-amber-600 hover:text-white'>Read More</Link>
       </div>
     </div>

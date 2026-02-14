2f17e2c8e0624270abee57054699c64d88c4b8d2
Nikunj Vadher <nikunjjv27@gmail.com>
2026-02-14 21:55:14 +0530

    add suspense and move api call to booklist page

--- app/(home)/components/BookList.tsx
+++ app/(home)/components/BookList.tsx
@@ -3,12 +3,17 @@ import Card from "./Card"
 
 
 
-const BookList = ({books}: {books: Book[]}) => {
+const BookList = async () => {
+  console.log(process.env.API_URL)
+  const response = await fetch(`${process.env.API_URL}/books`)
+
+  const books = await response.json();
+  console.log(books)
 
   return (
     <div className="grid grid-cols-1 gap-5 md:grid-cols-3 justify-items-center items-stretch auto-rows-fr container mx-auto py-5">
       {
-        books.map((book) => {
+        books.map((book : Book) => {
           return <Card key={book._id} book={book} />
         })
       }</div>
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,21 +1,19 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import Banner from './components/Banner'
 // import Card from './components/Card'
 import BookList from './components/BookList'
 
 const page = async () => {
 
-  console.log(process.env.API_URL)
-  const response = await fetch(`${process.env.API_URL}/books`)
-
-  const books = await response.json();
-  console.log(books)
 
   return (
     <div>
       <Banner />
       {/* <Card /> */}
-      <BookList books={books} />
+      <Suspense fallback={<div>Loading...</div>}>
+
+        <BookList />
+      </Suspense>
     </div>
   )
 }
